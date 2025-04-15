2d55ecde8e736cdbf145e1e2c3400ac39afff542
Nand.Devs <maaay.amanda@gmail.com>
2025-04-14 21:38:06 -0300

    Aulas de PHP

---
+++ handson-php/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeira aula em PHP</title>
+</head>
+<body>
+    <?php 
+    //Primeiro código.
+    echo "<h2 align='center'> Fiz meu primeiro código em PHP!!</h2>";
+    // O echo adiciona conteúdo a um arquivo.
+
+    echo "<h1 align= 'center' style = 'color: blue' > PHP - Personal Home Page </h1>";
+    echo "<p > O php é uma linguagem Server side, ou seja, ela consegue serve e atende os requisitos do cliente. Ela é Open Source, então pode ser compartilhada e outras pessoas podem contribuir
+    no código. Multiplataforma: roda em vários sistemas operacionais. É uma linguagem dinâmica, não é necessário declarar os tipos de variáveis. Seu principal framework é o laravel e suas melhores versões são a 7 e a 8. </p>"
+    ?>
+</body>
+</html>
