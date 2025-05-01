0662eeac0688f8e1a6651ac5075834faac37145d
Nand.Devs <maaay.amanda@gmail.com>
2025-05-01 14:45:51 -0300

    Delete AtividadePwII_Site directory

--- AtividadePwII_Site/php/conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    // Para criar uma variável em php, utilizamos o caracter especial $
-    $username = "root";
-    $password = "";
-
-    $pdo = new PDO('mysql:host=localhost;dbname=db_pw2', $username, $password);
-?>
